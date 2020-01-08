e3563df5672daaa580dce4ea9b9433f2a05fc753
raysayantan <ray.sayantan@gmail.com>
2020-01-08 22:20:35 +0530

    Add files via upload
    Spring Application with OAuth Authentication

---
+++ springsecurity-facebook-login/src/test/java/io/springsecurity/springsecurityfacebooklogin/SpringsecurityFacebookLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package io.springsecurity.springsecurityfacebooklogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityFacebookLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
