95fa491c4d30622ed63517585f6c1eab47a07493
raysayantan <ray.sayantan@gmail.com>
2020-02-27 12:30:01 +0530

    Create Product.java

---
+++ BasicResrWebService/productrestapidemo/src/java/com/myexample/productrestapidemo/entities/Product.java
@@ -0,0 +1,42 @@
+package com.myexample.productrestapidemo.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String description;
+	private int price;
+	
+	public int getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+}
