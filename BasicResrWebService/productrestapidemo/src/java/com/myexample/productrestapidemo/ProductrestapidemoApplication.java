4dfe7c5da2593c9b16bd44e77cfb065e3b165801
raysayantan <ray.sayantan@gmail.com>
2020-02-27 12:28:08 +0530

    Create ProductrestapidemoApplication.java

---
+++ BasicResrWebService/productrestapidemo/src/java/com/myexample/productrestapidemo/ProductrestapidemoApplication.java
@@ -0,0 +1,13 @@
+package com.myexample.productrestapidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductrestapidemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductrestapidemoApplication.class, args);
+	}
+
+}
