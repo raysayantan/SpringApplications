9bce8bb422b176cbbf80dabf82ec52596f5bcd44
raysayantan <ray.sayantan@gmail.com>
2020-02-27 12:45:40 +0530

    Create ProductrestapidemoApplicationTests.java

---
+++ BasicResrWebService/productrestapidemo/src/test/java/com/myexample/productrestapidemo/ProductrestapidemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myexample.productrestapidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductrestapidemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
